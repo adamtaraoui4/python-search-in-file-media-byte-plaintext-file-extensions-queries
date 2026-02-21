43431bc749abd4e592da3d34747d6a8575ad971c
adamtaraoui4 <adam.taraoui.adam@atomicmail.io>
2026-02-21 21:41:26 +0100

    Add files via upload

---
+++ searchFileOrFolderOrMediaName.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Founder's firstnam : Adam
+Founder's lastname : Taraoui
+Founder's date of birth : 01/01/1990
+Founder's place of birth : casablanca
+Founder's company's name : zoom business intelligence
+Founder's program's patent : GNU-GPG
+Founder's program's description : 
+Program to localize a file name a directory name between 
+a list of files and directories in a certain concrete directory or an abstract one.
+If the query is found in a list of file or directories certain file is found, 
+the found query is given in the certain line 
+of the concrete directory path of the file.
+"""
+import os # importing the operating system
+filesListSameNameAsQuery = [] # initialzing files name list with a name to fill it out
+directoriesSameNameAsQuery = [] # initialzing directories name list with a name to fill it out
+queryFound = False # setting up a boolean as query found not, to set it true if the query is found.
+#'./' means current directory path, the loop is looking for all the '.txt' and '.log' extensions.
+query = raw_input(str("Give a query name to look for : > ")) # selecting a query input
+for root, dirs, files in os.walk('./'): # using os.walk function to walk through the given rootpath files and directories
+    #print dirs
+    #files
+    for file in files: # looping through the files
+        if query.strip() in file: # comparing the query name to the files names
+            queryFound = True # setting query found is true
+            full_path = os.path.abspath(os.path.join(root, file)) # making the variable full path of the file
+            filesListSameNameAsQuery.append(full_path) # adding the file path to the directory
+            #print "Full path appended : " + full_path
+    for dircs in dirs: # looping through the files
+        if query.strip() in dircs: # comparing the query name to the directories names
+            queryFound = True # setting query found is true
+            full_path = os.path.abspath(os.path.join(root, dircs)) # making the variable full path of the directory
+            directoriesSameNameAsQuery.append(full_path)  # adding the directory path to the directory
+            #print "Full path appended : " + full_path
+if queryFound: # boolean(variable with true or false as choice) giving true if foldername, filename is found or some of the letters found in a certain foldername or filename.
+    print ""
+    print "The query was found : "
+    print "----------------------"
+    print "files :"
+    print "----------------------"
+    n = 1
+    for x in filesListSameNameAsQuery: # printing files list
+    	print str(n) + " : " + x
+        n+=1
+    print "----------------------"
+    print "directories :"
+    print "----------------------"
+    n = 1
+    for y in directoriesSameNameAsQuery: # printing directories list
+    	print str(n) + " : " + x
+        n+=1    	
+else:
+    print "The query was not found."
+###########################################################
+###########################################################
+###########################################################
+# Use minimum a letter to not get an error while looking for minimum a single character.
+# place this file between files and folders where to look for to get an abstract path to the folder, a topmost place for a better tree structure without complications.
+# Thank you in advance.
+###########################################################
+###########################################################
+###########################################################
---
+++ serachForByteFileisTheSameAsOtherByteFile.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Founder's firstnam : Adam
+Founder's lastname : Taraoui
+Founder's date of birth : 01/01/1990
+Founder's place of birth : casablanca
+Founder's company's name : zoom business intelligence
+Founder's program's patent : GNU-GPG
+Founder's program's description : 
+Program to localize a query between 
+files in a certain concrete directory or an abstract one.
+If the query in a certain file is found, 
+the found query is given in the certain line 
+of the concrete directory path of the file
+file byte the same as another byte file.
+"""
+import os # importing the operating system
+filesEndsWithDotTxtOrDotLogExtension = [] # initialzing a list with a name to fill it out
+#'./' means current directory path, the loop is looking for all the '.txt' and '.log' extensions.
+for root, dirs, files in os.walk('./'): # using os.walk function to walk through the given rootpath files and directories
+    for file in files: # looping through the files
+        if file.endswith('.jpeg') or file.endswith('.png') or file.endswith('.jpg') or file.endswith('.gif'): # condition, if the files endswith '.txt', '.log'
+            # Use os.path.join to get the full path
+            full_path = os.path.abspath(os.path.join(root, file)) # making the variable full path of the directory
+            filesEndsWithDotTxtOrDotLogExtension.append(full_path) 
+            #print "Full path appended : " + full_path
+query = raw_input(str("Give an input(file) to look for in  byte files in a certain path : > ")) # selecting a query input
+queryReadedFile = open(query,'rb') # read the files already selected by the previous lines
+queryReadedFilesReadlines = readedFile.read() #reading the lines of the files
+queryFound = False # setting up a boolean as query found not, to set it true if the query is found.
+queryFoundList = [] # initializing an empty list to fill up the list
+for xFile in filesEndsWithDotTxtOrDotLogExtension: # looping through the files
+    readedFile = open(xFile,'rb') # read the files already selected by the previous lines
+    readedFilesReadlines = readedFile.read() #reading the lines of the files
+    lineNumber = 0 # initializing the line numbers to zero
+    for xLine in readedFilesReadlines: # looping every line of every line ang looking for the query
+        if queryReadedFilesReadlines in xLine: # condition if the query is found in the line
+            queryFound = True # setting up the variable as queryFound True
+            queryFoundList.append("fileName : " + str(xFile) + " LineNumber : " + str(lineNumber) + " query found.") # appending to already an initialized list the line of the file found in query.
+        lineNumber+=1 # incrementing the line numbers
+    lineNumber = 0 # initialization of a lineNumber to zero
+    readedFile.close() # close the file
+if queryFound: # if the query is already found just one time
+    print "Query found in : " # print on the screen the query is found
+    n = 1
+    for x in queryFoundList: # printing the list of the files where the query is found at
+	print str(n) + " : " + x
+    	n+=1
+else: # elsewhere from the last condition
+    print "Query not found" # printing out, the query was not found
+###########################################################
+###########################################################
+###########################################################
+
+###########################################################
+###########################################################
+###########################################################
